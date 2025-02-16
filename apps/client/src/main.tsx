e139023bd071365e22d7209f90fa48b3ec3bebc8
tzlan <talzana55@gmail.com>
2025-02-16 17:06:08 +0200

    Fix on app / arrow function

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import App from './app/app';
+import { App } from './app/app';
 import { BrowserRouter } from 'react-router-dom';
 
 
