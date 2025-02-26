e78bcbd82e895a37994597a11cb547b0643e5eeb
tzlan <talzana55@gmail.com>
2025-02-26 10:21:36 +0200

    (client) fix bug configuration

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,21 +1,26 @@
-import { AdminEditUser, AdminNewToast, Login ,NotFound , UserRegistration } from "../components";
+import {
+  AdminEditUser,
+  AdminNewToast,
+  Login,
+  NotFound,
+  UserRegistration,
+} from '../components';
 import { Routes, Route } from 'react-router-dom';
-import { NewToast } from "../components/new-toast";
+import { NewToast } from '../components/new-toast';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      
+
       <Route path="/admin-new-toast" element={<AdminNewToast />} />
-      <Route path="/admin-edit-user" element={<AdminEditUser/>}/>
+      <Route path="/admin-edit-user" element={<AdminEditUser />} />
 
       <Route path="/user-registration" element={<UserRegistration />} />
       <Route path="/new-toast" element={<NewToast />} />
-      <Route path="/edit-user" element={<AdminEditUser/>}/>
-  
-      <Route path='*' element={<NotFound/>} />
-   
+      <Route path="/edit-user" element={<AdminEditUser />} />
+
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
-}
+};
--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -15,3 +15,20 @@ module.exports = {
   },
   plugins: [],
 };
+
+/**
+ * quand j'ai compris que mon projet chargait dans le vide pendant 3 h (ce message (base) punisher@MacBook-du-Punisher toast-project % npx nx serve client
+(node:34812) ExperimentalWarning: CommonJS module /opt/homebrew/lib/node_modules/npm/node_modules/debug/src/node.js is loading ES Module /opt/homebrew/lib/node_modules/npm/node_modules/supports-color/index.js using require().
+Support for loading ES Module in require() is an experimental feature and might change at any time
+(Use `node --trace-warnings ...` to show where the warning was created)
+The CJS build of Vite's Node API is deprecated. See https://vite.dev/guide/troubleshooting.html#vite-cjs-node-api-deprecated for more details.
+⠏ Creating project graph dependencies with 7 plugins) j'ai crée un nouveau projet nx et j'ai juste copié mon dossier
+ * components et installé quelques bibliotheques et ensuite cela fonctionnait mon serveur affichait ma page
+ * le probleme est que je veux que mon projet s'execute sur mon projet initial (exemple lien avec github..)
+ * j'aimerais comprendre comment je peux faire cela
+ * je me dis que c'est seulement une question de configuration et d'installations 
+ * j'ai les 2 projets en face de moi
+ * le client est le meme 
+ * dans un projet il se lance et dans l'autre il m'affiche le message plus haut
+ * 
+ *  */ 
