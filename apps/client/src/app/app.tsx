7eadaed61e22d78f5bfce658e55d5398832acf87
tzlan <talzana55@gmail.com>
2025-03-17 11:34:19 +0200

    Fix cr Yahalom

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -14,25 +14,18 @@ import { Criminal } from '../components/criminal';
 import { Dashboard } from '../components/dashboard';
 import { AdminDashboard } from '../components/admin-dashboard';
 
-export const App = () => {
+export const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
-
       <Route path="/login" element={<Login />} />
-
-      {/**Admin routes */}
       <Route path="/admin-new-toast" element={<AdminNewToast />} />
       <Route path="/admin-edit-user" element={<AdminEditUser />} />
       <Route path="/edit-user" element={<AdminEditUser />} />
       <Route path="/admin-dashboard" element={<AdminDashboard />} />
-
-      {/**Form routes */}
       <Route path="/user-registration" element={<UserRegistration />} />
       <Route path="/new-toast" element={<NewToast />} />
       <Route path="/dashboard" element={<Dashboard />} />
-
-      {/**Routes */}
       <Route path="/record-page" element={<RecordPage />} />
       <Route path="/toast-past" element={<ToastPast />} />
       <Route path="/criminal" element={<Criminal />} />
