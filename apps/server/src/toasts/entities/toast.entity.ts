c5ba8474b7ce62acd99513cc8d60e9c89c0e4dcc
tzlan <talzana55@gmail.com>
2025-03-25 17:04:14 +0200

    DB sequelize create dababase

---
+++ apps/server/src/toasts/entities/toast.entity.ts
@@ -0,0 +1,63 @@
+import {
+  Column,
+  Model,
+  Table,
+  DataType,
+  PrimaryKey,
+  ForeignKey,
+  BelongsTo,
+} from 'sequelize-typescript';
+import { User } from '../../users/entities/user.entity';
+
+@Table({ tableName: 'toasts', paranoid: true })
+export class Toast extends Model<Toast> {
+  @PrimaryKey
+  @Column({
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV4,
+  })
+  override id!: string;
+
+
+
+  @ForeignKey(() => User)
+  @Column({
+    type: DataType.UUID,  
+    allowNull: false,
+    field: 'user_id',  
+  })
+  userId!: string;  
+
+  @BelongsTo(() => User, { foreignKey: 'userId' })
+  user!: User;
+
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    field: 'description',
+  })
+  description!: string;
+
+  @Column({
+    type: DataType.DATE,
+    allowNull: false,
+    field: 'date',
+  })
+  date!: Date;
+
+  @Column({
+    type: DataType.ENUM('CANCELED', 'DELAYED', 'ON TIME'),
+    allowNull: false,
+    field: 'statustoast',
+  })
+  statusToast!: 'CANCELED' | 'DELAYED' | 'ON TIME';
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    field: 'place',
+  })
+  place!: string;
+}
+
